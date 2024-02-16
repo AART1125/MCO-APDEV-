1fe762d3eed6875e182f5e7b68877c888cf924b1
jlnsznnn <140251032+jlnsznnn@users.noreply.github.com>
2024-02-16 11:31:15 +0800

    small changes reply.js

--- restaurant-review-replies/reply.js
+++ restaurant-review-replies/reply.js
@@ -72,23 +72,13 @@ document.querySelectorAll(".rev2").forEach(review => {
 
 document.addEventListener('DOMContentLoaded', function () {
     var replyButton1 = document.getElementById('reply1');
-    var replyButton2 = document.getElementById('reply2');
     var cancelButton1 = document.getElementById('cancel-1');
-    var cancelButton2 = document.getElementById('cancel-2');
     var sendButton1 = document.getElementById('send-1');
-    var sendButton2 = document.getElementById('send-2');
     var textarea1 = document.getElementById('reply-txtarea-1');
-    var textarea2 = document.getElementById('reply-txtarea-2');
 
     replyButton1.addEventListener('click', function(event) {
         event.preventDefault();
         document.getElementById('reply-pop-up-1').style.display = 'block';
-
-    })
-
-    replyButton2.addEventListener('click', function(event) {
-        event.preventDefault();
-        document.getElementById('reply-pop-up-2').style.display = 'block';
 
     })
 
@@ -98,26 +88,12 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('reply-pop-up-1').style.display = 'none';
     })
 
-    cancelButton2.addEventListener('click', function(event) {
-        event.preventDefault();
-        textarea2.value = "";
-        document.getElementById('reply-pop-up-2').style.display = 'none';
-    })
 
     sendButton1.addEventListener('click', function(event) {
         event.preventDefault();
         textarea1.value = "";
         document.getElementById('reply-pop-up-1').style.display = 'none';
     })
-
-    sendButton2.addEventListener('click', function(event) {
-        event.preventDefault();
-        textarea2.value = "";
-        document.getElementById('reply-pop-up-2').style.display = 'none';
-    })
-
-    
-
 });
 
 var editLinks = document.getElementsByClassName("edit");
