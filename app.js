23557531f565ad87f9f27f5725a62e410fc4f345
AARTYOON <aaron_toledo@dlsu.edu.ph>
2024-03-05 22:25:39 +0800

    Updated the newly added files as a good start

--- app.js
+++ app.js
@@ -18,7 +18,17 @@ mongoose.connect('mongodb://localhost:27017/logindb');
 
 server.get('/', function(req, resp){
     resp.render('main',{
-      layout: 'index',
-      title: 'My Home page'
+        layout: 'index',
+        title: 'My Home page'
     });
-  });
+    console.log("Successful Render");
+});
+
+process.on('SIGTERM',finalClose);  //general termination signal
+process.on('SIGINT',finalClose);   //catches when ctrl + c is used
+process.on('SIGQUIT', finalClose); //catches other termination commands
+
+const port = process.env.PORT | 3000;
+server.listen(port, function(){
+    console.log('Listening at port '+port);
+});
