032d3a50b65f3b40e9c9fe8fa16ac74408455ee6
jlnsznnn <140251032+jlnsznnn@users.noreply.github.com>
2024-03-10 00:38:58 +0800

    added restaurantModel.js

--- Model/restaurantModel.js
+++ Model/restaurantModel.js
@@ -1,1 +1,41 @@
 const schemas = require('./schemaModels');
+
+async function getRestaurantData() {
+    try {
+        const restaurants = await restaurantModel.find({});
+        const restaurantDataArray = [];
+
+        restaurants.forEach(restaurant => {
+            const restaurantData = {
+                'resto-img': restaurant.restoimg[0],
+                'resto-name': restaurant.restoname,
+                'resto-type': restaurant.restotype,
+                'location': restaurant.location,
+                'address': restaurant.address,
+                'opening-hours': restaurant.openingHours,
+                'reviews': restaurant.reviews.map(review => ({
+                    'user-profileimg': review.users_id.profileimg,
+                    'user-fullname': review.users_id.fullname,
+                    'user-username': review.users_id.username,
+                    'likes': review.likes,
+                    'dislikes': review.dislikes,
+                    'is-recommend': review.isRecommend,
+                    'review': review.review,
+                    'reply': review.reply, 
+                    'owner-profileimg': restaurant.owner_id.profileimg,
+                    'owner-fullname': restaurant.owner_id.fullname,
+                    'owner-username': restaurant.owner_id.username,
+                })),
+            };
+            restaurantDataArray.push(restaurantData);
+        });
+        return restaurantDataArray;
+    } catch (error) {
+        console.error('Error fetching restaurant data:', error);
+        throw error;
+    }
+}
+
+module.exports = {
+    getRestaurantData
+}
