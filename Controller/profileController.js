b09dbb21cd0abb2bf44571f4efc83b17f8cd52c1
janina <132965455+janina-esteban@users.noreply.github.com>
2024-03-09 22:10:42 +0800

    yihee

--- Controller/profileController.js
+++ Controller/profileController.js
@@ -1,7 +1,5 @@
 const profileDB = require('../Model/profileModel');
 
-// to fix pa
-
 function add(server){
     server.post('/user-profile', (req, resp) => {
         profileDB.findProfile(req, resp);
