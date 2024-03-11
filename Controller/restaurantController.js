f1061e2b6fde7f5fec72cae308552a743c0a1d16
jlnsznnn <140251032+jlnsznnn@users.noreply.github.com>
2024-03-11 21:03:35 +0800

    not fully working

--- Controller/restaurantController.js
+++ Controller/restaurantController.js
@@ -1,8 +1,30 @@
 const restaurantModel = require('../Model/restaurantModel'); 
 
 function add(server) {
-    server.get('/restaurant/Bonchon', (req, resp) => {
-        resp.send('Testing'); 
+    server.get('/restaurant/:restoname', async (req, resp) => {
+        try {
+            const restaurantDataArray = await restaurantModel.getRestaurantData();
+            
+            const restonameParam = req.params.restoname;
+            const selectedRestaurant = restaurantDataArray.find(restaurant => restaurant['resto-name'] === restonameParam);
+
+            if (selectedRestaurant) {
+                resp.render('restaurant', {
+                    layout: 'index',
+                    title: 'Archer\'s Hunt',
+                    js: '/common/js/restaurant.js', 
+                    css: '/common/css/restaurant.css', 
+                    islogin: false,
+                    isOwner: false,
+                    restaurant: selectedRestaurant
+                });
+            } else {
+                resp.status(404).send('Restaurant not found'); 
+            }
+        } catch (error) {
+            console.error('Error in /restaurant/:restoname route:', error);
+            resp.status(500).send('Internal Server Error');
+        }
     });
 }
 
