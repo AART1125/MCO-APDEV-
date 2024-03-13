5a88827410647330badcacfd570c8ed537e75af9
AARTYOON <aaron_toledo@dlsu.edu.ph>
2024-03-13 19:26:12 +0800

    fix formatting

--- Controller/loginController.js
+++ Controller/loginController.js
@@ -7,16 +7,16 @@ function add(server){
     });
 
     server.get('/main-user', async (req,resp) => {
-            const restaurants = await getRestaurantData();
-            resp.render('main', {
-                layout: 'index',
-                title: 'Archer\'s Hunt',
-                js: '/common/js/mainFunc.js',
-                css: '/common/css/main.css',
-                islogin: true,
-                isOwner: false,
-                restaurants: restaurants
-            });
+        const restaurants = await getRestaurantData();
+        resp.render('main', {
+            layout: 'index',
+            title: 'Archer\'s Hunt',
+            js: '/common/js/mainFunc.js',
+            css: '/common/css/main.css',
+            islogin: true,
+            isOwner: false,
+            restaurants: restaurants
+        });
     });
 
     server.get('/main-owner', async (req,resp) => {
--- app.js
+++ app.js
@@ -50,6 +50,16 @@ for (let path of controllerPaths) {
   controller.add(server);
 }
 
+function finalClose(){
+  console.log('Close connection at the end!');
+  mongoose.connection.close();
+  process.exit();
+}
+
+process.on('SIGTERM',finalClose);  //general termination signal
+process.on('SIGINT',finalClose);   //catches when ctrl + c is used
+process.on('SIGQUIT', finalClose); //catches other termination commands
+
 const port = process.env.PORT | 3000;
 server.listen(port, function () {
   console.log("Listening at port " + port);
