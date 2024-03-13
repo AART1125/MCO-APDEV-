c81c70c610113975f911b9d1dff47b13496dbb42
AARTYOON <aaron_toledo@dlsu.edu.ph>
2024-03-13 17:38:55 +0800

    Made Login function asynchronous

--- Model/loginModel.js
+++ Model/loginModel.js
@@ -1,33 +1,26 @@
 //Database Functions
 const schemas = require('./schemaModels');
 
-function loginToWebsite(req, resp){
+async function loginToWebsite(req, resp){
     const query = {username : req.body.username, password : req.body.password};
     let response = {doesExist : true, isOwner : false};
 
-    console.log('Trying to Login');
-    schemas.userModel.findOne(query).then((login) => {
-        console.log('Finding user');
-        if (login != undefined && login._id != null) {
-            console.log("User found!");
-            resp.send(response);
-        } else {
-            console.log("User not found!");
-            schemas.ownerModel.findOne(query).then((owner) => {
-                console.log('Finding owner');
-                if (owner != undefined && owner._id != null) {
-                    console.log("Owner found!");
-                    response = {doesExist : true, isOwner : true};
-                    resp.send(response);
-                } else {
-                    console.log('Owner not found!');
-                    console.log('No information in Database!');
-                    response = {doesExist : false};
-                    resp.send(response);
-                };
-            }).catch();
-        };
-    }).catch();
+    const user = await schemas.userModel.findOne(query);
+    const owner = await schemas.ownerModel.findOne(query);
+
+    if(user && !owner){
+        console.log("User found!");
+        
+    } else if (!user && owner){
+        console.log("Owner found!");
+        response = {doesExist : true, isOwner : true};
+    } else {
+        console.log('No information in Database!');
+        response = {doesExist : false, isOwner : false};
+    }
+
+    resp.send(response);
+
 }
 
 module.exports = {
