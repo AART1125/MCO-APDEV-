b8d5af6bac7cd0dd0d07a584d7e402f0a3b2c4be
AARTYOON <aaron_toledo@dlsu.edu.ph>
2024-04-01 20:48:55 +0800

    trying something for render

--- Model/loginModel.js
+++ Model/loginModel.js
@@ -10,37 +10,44 @@ async function loginToWebsite(req, resp){
     const owner = await schemas.ownerModel.findOne(query);
 
     if(user && !owner){
-        const out = bcrypt.compare(req.body.password, user.password);
+        bcrypt.compare(req.body.password, user.password).then(out => {
+            console.log(out)
+            if (!out) {
+                resp.send({doesExist : false});
+                console.log("Wrong Password!");
+            } else {
+                req.session.login_user = user._id;
+                req.session.login_username = user.username;
+                req.session.login_isOwner = false;
+                req.session.login_id = req.sessionID;
+                console.log("Status: Success");
+                resp.send(response);
+            }
+        }).catch();
 
-        if (!out) {
-            console.log("Wrong Password!");
-        } else {
-            req.session.login_user = user._id;
-            req.session.login_username = user.username;
-            req.session.login_isOwner = false;
-            req.session.login_id = req.sessionID;
-            console.log("Status: Success");
-        }
+        
     
     } else if (!user && owner){
-        const out = bcrypt.compare(req.body.password, owner.password);
+        bcrypt.compare(req.body.password, owner.password).then(out => {
+            if (!out) {
+                resp.send({doesExist : false});
+                console.log("Wrong Password!");
+            } else {
+                req.session.login_user = owner._id;
+                req.session.login_username = owner.username;
+                req.session.login_isOwner = true;
+                req.session.login_id = req.sessionID;
+                console.log("Owner found!");
+                resp.send({doesExist : true, isOwner : true});
+            }
+        }).catch();
 
-        if (!out) {
-            console.log("Wrong Password!");
-        } else {
-            req.session.login_user = owner._id;
-            req.session.login_username = owner.username;
-            req.session.login_isOwner = true;
-            req.session.login_id = req.sessionID;
-            console.log("Owner found!");
-            response = {doesExist : true, isOwner : true};
-        }
+        
     } else {
         console.log('No information in Database!');
-        response = {doesExist : false};
+        resp.send({doesExist : false});
+        
     }
-
-    resp.send(response);
 
 }
 
