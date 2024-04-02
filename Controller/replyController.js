40aeffbc165589109e4b51f4cc97e8ba32f044ac
jlnsznnn <140251032+jlnsznnn@users.noreply.github.com>
2024-04-02 19:51:18 +0800

    made reply.hbs instead of popup

---
+++ Controller/replyController.js
@@ -0,0 +1,21 @@
+const review = require('../Model/reviewModel');
+
+function add(server) {
+    server.get('/restaurant/:restoname/reply/:reviewId', (req, resp) => {
+        console.log('test');
+        resp.render('reply', {
+            layout: 'index',
+            title: 'Reply to a Review',
+            js: '/common/js/reply.js',
+            css: '/common/css/review.css',
+            restoname: req.params.restoname,
+            islogin: req.session.login_id != undefined,
+            isOwner: req.session.login_isOwner,
+            username: req.session.login_username,
+        });
+    });
+}
+
+module.exports = {
+    add
+}
