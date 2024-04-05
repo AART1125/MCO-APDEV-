725657ccc4b1bba867b418c1b89542b732a048b7
janina <132965455+janina-esteban@users.noreply.github.com>
2024-04-05 19:51:44 +0800

    add/delete prends

--- Controller/profileController.js
+++ Controller/profileController.js
@@ -1,7 +1,13 @@
-const { findUserProfile, findOwnerProfile, findUserProfileEdit, UserProfileEdit, findOwnerProfileEdit, OwnerProfileEdit, getResto, getRestoEdit, addRestaurant, editRestaurant, deleteRestaurant } = require('../Model/profileModel');
+const { findUserProfile, findOwnerProfile, findUserProfileEdit, 
+    UserProfileEdit, findOwnerProfileEdit, OwnerProfileEdit, 
+    getResto, getRestoEdit, 
+    addRestaurant, editRestaurant, deleteRestaurant,
+    addFriend, deleteFriend} = require('../Model/profileModel');
 
 function add(server) {
     
+// ALL GETS
+
     server.get('/user-profile/:username', async (req, resp) => {
         try {
             await findUserProfile(req, resp, 'profile');
@@ -20,27 +26,9 @@ function add(server) {
         }
     });
 
-    server.post('/user-profile/:username/edit-user-profile', async (req, resp) => {
-        try {
-            await UserProfileEdit(req, resp);
-        } catch (error) {
-            console.error('Error getting profile:', error);
-            resp.status(500).send('Error getting profile');
-        }
-    });
-
     server.get('/owner-profile/:username/edit-owner-profile', async (req, resp) => {
         try {
             await findOwnerProfileEdit(req, resp);
-        } catch (error) {
-            console.error('Error getting profile:', error);
-            resp.status(500).send('Error getting profile');
-        }
-    });
-
-    server.post('/owner-profile/:username/edit-owner-profile', async (req, resp) => {
-        try {
-            await OwnerProfileEdit(req, resp);
         } catch (error) {
             console.error('Error getting profile:', error);
             resp.status(500).send('Error getting profile');
@@ -83,6 +71,44 @@ function add(server) {
         }
     });    
 
+    server.get('/user-profile/:username/add-friend/:friendUsername', async (req, resp) => {
+        try {
+            await addFriend(req, resp);
+        } catch (error) {
+            console.error('Error in route handler:', error);
+            resp.status(500).send("Internal Server Error");
+        }
+    }); 
+
+    server.get('/user-profile/:username/delete-friend/:friendUsername', async (req, resp) => {
+        try {
+            await deleteFriend(req, resp);
+        } catch (error) {
+            console.error('Error deleting friend:', error);
+            resp.status(500).send('Error deleting friend');
+        }
+    });    
+
+// ALL POSTS
+
+    server.post('/user-profile/:username/edit-user-profile', async (req, resp) => {
+        try {
+            await UserProfileEdit(req, resp);
+        } catch (error) {
+            console.error('Error getting profile:', error);
+            resp.status(500).send('Error getting profile');
+        }
+    });
+
+    server.post('/owner-profile/:username/edit-owner-profile', async (req, resp) => {
+        try {
+            await OwnerProfileEdit(req, resp);
+        } catch (error) {
+            console.error('Error getting profile:', error);
+            resp.status(500).send('Error getting profile');
+        }
+    });
+
     server.post('/owner-profile/:username/add-restaurant', async (req, resp) => {
         try {
             await addRestaurant(req, resp);
@@ -109,6 +135,8 @@ function add(server) {
             resp.status(500).send('Error editing restaurant');
         }
     });    
+
+    
 
 }
 
